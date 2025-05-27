900f114cfab79dad0b7ae02be0df62c6f19a7fe7
John Robinson <john@bayfrontmedia.com>
2025-05-27 01:29:13 -0400

    v1.1.0 (#2)
    * Updated HasNullableJsonField trait
    
    * Updated parameter name
    
    * Bugfix
    
    * Changelog update

--- src/Traits/HasNullableJsonField.php
+++ src/Traits/HasNullableJsonField.php
@@ -12,6 +12,13 @@ trait HasNullableJsonField
 {
 
     /**
+     * Nullable JSON field.
+     *
+     * @return string
+     */
+    abstract protected function getNullableJsonField(): string;
+
+    /**
      * Define nullable JSON field array, removing keys with null values.
      *
      * @param array $array
@@ -39,19 +46,24 @@ trait HasNullableJsonField
      * @param string $table_name
      * @param string $primary_key_field
      * @param mixed $primary_key
+     * @param string $json_field
      * @param array $array
      * @return array
      */
-    protected function updateNullableJsonField(OrmService $ormService, string $table_name, string $primary_key_field, mixed $primary_key, array $array): array
+    protected function updateNullableJsonField(OrmService $ormService, string $table_name, string $primary_key_field, mixed $primary_key, string $json_field, array $array): array
     {
 
-        $meta = $ormService->db->single("SELECT $primary_key_field FROM $table_name WHERE $primary_key_field = :id", [
+        $meta = $ormService->db->single("SELECT $json_field FROM $table_name WHERE $primary_key_field = :id", [
             'id' => $primary_key
         ]);
 
-        $meta_dot = json_decode($meta, true);
+        if (is_string($meta)) {
+            $meta_dot = json_decode($meta, true);
+        } else {
+            $meta_dot = [];
+        }
 
-        if (is_array($meta_dot)) {
+        if (is_array($meta_dot)) { // Checks decode was successful
             $meta = array_merge(Arr::dot($meta_dot), Arr::dot($array));
         } else {
             $meta = Arr::dot($array);
